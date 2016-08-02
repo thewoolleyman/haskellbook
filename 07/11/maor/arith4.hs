adbda0fb701713b6747a4b054fe90ded592a9293
Maor Leger <maor.leger@gmail.com>
2016-08-02 01:53:57 +0000

    now really finished chapter 7 exercises

--- 07/11/maor/arith4.hs
+++ 07/11/maor/arith4.hs
@@ -1,8 +1,8 @@
 module Arith4 where
 
-roundTrip :: (Show a, Read a) => a -> a
+roundTrip :: (Show a, Read b) => a -> b
 roundTrip = read . show
 
 main = do
-  print (roundTrip 4)
+  print (roundTrip 4 :: Integer)
   print (id 4)
