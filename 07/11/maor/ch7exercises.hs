8b49ce48840c3bf737c9a8817e5fffe569039678
Maor Leger <maor.leger@gmail.com>
2016-08-02 01:52:20 +0000

    finished chapter 7 exercises

--- 07/11/maor/ch7exercises.hs
+++ 07/11/maor/ch7exercises.hs
@@ -9,20 +9,42 @@ module Chapter7Exercises where
   5. u
 -}
 
+tensDigit :: Integer -> Integer
 tensDigit x = d
   where xLast = div x 10
         d     = mod xLast 10
 
+tensDigit' :: Integer -> Integer
 tensDigit' x = d
   where (xLast, _) = divMod x 10
         (_, d) = divMod xLast 10
 
+tensDigitFnc :: Integer -> Integer
 tensDigitFnc x = mod (fst (divMod x 10)) 10
 
+tensDigitPF :: Integer -> Integer
 tensDigitPF = (a . b . c)
   where applyToTen f x = f x 10
         a = applyToTen mod 
         b = fst
         c = applyToTen divMod 
 
+applyTensDigit :: Integer -> Integer
 applyTensDigit x = tensDigitPF x
+
+hunsD :: Integer -> Integer
+hunsD x = applyTensDigit (div x 10) 
+
+foldBool :: a -> a -> Bool -> a
+foldBool x y b
+  | b == True   = x
+  | b == False  = y
+
+foldBool2 :: a -> a -> Bool -> a
+foldBool2 x y b =
+  case b of
+    True -> x
+    False -> y
+
+g :: (a -> b) -> (a, c) -> (b, c)
+g f (a, c) = (f a, c)
