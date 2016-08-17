a91d6828c7e2fb0e7fbd2e11fa2435bc93890fdc
Maor Leger <maor.leger@gmail.com>
2016-08-17 19:00:56 +0000

    more ch10 exercises

--- 10/10/maor/ch10exercises.hs
+++ 10/10/maor/ch10exercises.hs
@@ -36,3 +36,36 @@ myElem' :: Eq a => a -> [a] -> Bool
 myElem' elem = foldr
           (\x acc -> x == elem || acc) False
 
+myElem'' :: Eq a => a -> [a] -> Bool
+myElem'' elem = myAny' ((==) elem) 
+
+myReverse :: [a] -> [a]
+myReverse [] = []
+myReverse (x:xs) = (myReverse xs) ++ [x]
+
+myReverse' :: [a] -> [a]
+myReverse' = foldl (flip (:)) []
+
+myMap :: (a -> b) -> [a] -> [b]
+myMap f = foldr (\x acc -> (f x) : acc)  []
+
+myFilter :: (a ->Bool) -> [a] -> [a]
+myFilter f = foldr (\x acc -> case (f x) of
+                              True -> x : acc
+                              _ -> acc) []
+
+squish :: [[a]] -> [a]
+squish = foldr (\x acc -> x ++ acc) []
+
+squishMap :: (a -> [b]) -> [a] -> [b]
+squishMap f = foldr (\x acc -> (f x) ++ acc) []
+
+squishAgain :: [[a]] -> [a]
+squishAgain = squishMap id
+
+myMaximumBy :: (a -> a -> Ordering) -> [a] -> a
+myMaximumBy _ [] = undefined
+myMaximumBy f (x:xs) = foldr (\x acc -> case (f x acc) of
+                                  GT -> x
+                                  _ -> acc) x
+
