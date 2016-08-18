48433008eb816bfd70c530da40247c0a1bc679c9
Eric Dattore <edattore@gmail.com>
2016-08-17 23:41:26 -0600

    add traversal tests for binary tree

--- 11/15/eric/BinaryTree.hs
+++ 11/15/eric/BinaryTree.hs
@@ -30,3 +30,45 @@ mapOkay =
     print "Test Passed!"
   else
     error "Test Failed!"
+
+preorder :: BinaryTree a -> [a]
+preorder Leaf = []
+preorder (Node left a right) = [a] ++ preorder left ++ preorder right
+
+inorder :: BinaryTree a -> [a]
+inorder Leaf = []
+inorder (Node left a right) = inorder left ++ [a] ++ inorder right
+
+postorder :: BinaryTree a -> [a]
+postorder Leaf = []
+postorder (Node left a right) = postorder left ++ postorder right ++ [a]
+
+testTree :: BinaryTree Integer
+testTree = Node (Node Leaf 1 Leaf) 2 (Node Leaf 3 Leaf)
+
+testPreorder :: IO ()
+testPreorder =
+  if preorder testTree == [2, 1, 3] then
+    print "Test passed"
+  else
+    error "Test failed"
+
+testInorder :: IO ()
+testInorder =
+  if inorder testTree == [1, 2, 3] then
+    print "Test passed"
+  else
+    error "Test failed"
+
+testPostorder :: IO ()
+testPostorder =
+  if postorder testTree == [1, 3, 2] then
+    print "Test passed"
+  else
+    error "Test failed"
+
+main :: IO ()
+main = do
+  testPreorder
+  testInorder
+  testPostorder
