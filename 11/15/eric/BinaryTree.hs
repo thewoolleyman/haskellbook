21a217a750b32ff94922f56c67c79d23c46aa16c
Eric Dattore <edattore@gmail.com>
2016-08-17 19:50:09 -0600

    add skeleton for binary tree exercise

---
+++ 11/15/eric/BinaryTree.hs
@@ -0,0 +1,32 @@
+module BinaryTree where
+
+-- Node :: BinaryTree a -> a -> BinaryTree a -> BinaryTree a
+data BinaryTree a =
+    Leaf
+  | Node (BinaryTree a) a (BinaryTree a)
+  deriving (Eq, Show)
+
+insert' :: Ord a => a -> BinaryTree a -> BinaryTree a
+insert' b Leaf = Node Leaf b Leaf
+insert' b (Node left a right)
+  | b == a = Node left a right
+  | b < a = Node (insert' b left) a right
+  | b > a = Node left a (insert' b right)
+
+mapTree :: (a -> b) -> BinaryTree a -> BinaryTree b
+mapTree _ Leaf = Leaf
+mapTree f (Node left a right) =
+  Node undefined undefined undefined
+
+testTree' :: BinaryTree Integer
+testTree' =
+  Node (Node Leaf 3 Leaf) 1 (Node Leaf 4 Leaf)
+
+mapExpected =
+  Node (Node Leaf 4 Leaf) 2 (Node Leaf 5 Leaf)
+
+mapOkay =
+  if mapTree (+1) testTree' == mapExpected then
+    print "Test Passed!"
+  else
+    error "Test Failed!"
