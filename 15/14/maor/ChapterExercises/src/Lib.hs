62cf5ed48c5ee47ec31823e3581d7dface5b28df
Maor Leger <maor.leger@gmail.com>
2016-09-14 20:55:10 -0600

    more semigroup exercises

--- 15/14/maor/ChapterExercises/src/Lib.hs
+++ 15/14/maor/ChapterExercises/src/Lib.hs
@@ -1,11 +1,18 @@
 module Lib
     ( Trivial,
       Identity,
-      MyInt
+      MyInt,
+      Two,
+      Three,
+      Four,
+      BoolConj,
+      BoolDisj
     ) where
 
 import Data.Semigroup
 import Test.QuickCheck
+
+-- Trivial
 
 data Trivial = Trivial deriving (Eq, Show)
 
@@ -13,6 +20,9 @@ instance Semigroup Trivial where
   _ <> _ = Trivial
 
 instance Arbitrary Trivial where arbitrary = return Trivial
+
+
+-- Identity a 
 
 newtype Identity a = Identity a deriving (Show)
 
@@ -27,6 +37,8 @@ instance Arbitrary a => Arbitrary (Identity a) where
     a <- arbitrary
     return (Identity a)
 
+-- MyInt (just experimenting)
+
 newtype MyInt = MyInt Int deriving (Show)
 instance Semigroup MyInt where
   (MyInt a) <> (MyInt b) = MyInt (a + b)
@@ -39,6 +51,69 @@ instance Arbitrary MyInt where
     a <- arbitrary
     return (MyInt a)
 
+-- Two a b
+data Two a b = Two a b deriving (Eq, Show)
 
-someFunc :: IO ()
-someFunc = putStrLn "Hi"
+instance (Semigroup a, Semigroup b) => Semigroup (Two a b) where
+  (Two a b) <> (Two a' b') = Two (a <> a') (b <> b')
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Two a b) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    return (Two a b)
+
+-- Three a b c
+
+data Three a b c = Three a b c deriving (Eq, Show)
+
+instance (Semigroup a, Semigroup b, Semigroup c) => Semigroup (Three a b c) where
+  (Three a b c) <> (Three a' b' c') = Three (a <> a') (b <> b') (c <> c')
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c) => Arbitrary (Three a b c) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    c <- arbitrary
+    return (Three a b c)
+
+-- Four a b c d
+
+data Four a b c d = Four a b c d deriving (Eq, Show)
+
+instance (Semigroup a, Semigroup b, Semigroup c, Semigroup d) => Semigroup (Four a b c d) where
+  (Four a b c d) <> (Four a' b' c' d') = Four (a <> a') (b <> b') (c <> c') (d <> d')
+
+instance (Arbitrary a, Arbitrary b, Arbitrary c, Arbitrary d) => Arbitrary (Four a b c d) where
+  arbitrary = do
+    a <- arbitrary
+    b <- arbitrary
+    c <- arbitrary
+    d <- arbitrary
+    return (Four a b c d)
+
+
+-- BoolConj
+newtype BoolConj = BoolConj Bool deriving (Eq, Show)
+
+instance Semigroup BoolConj where
+  (BoolConj True) <> (BoolConj True) = BoolConj True
+  _ <> _ = BoolConj False
+
+instance Arbitrary BoolConj where 
+  arbitrary = do
+    a <- elements [False, True]
+    return (BoolConj a)
+
+-- BoolDisj
+newtype BoolDisj = BoolDisj Bool deriving (Eq, Show)
+
+instance Semigroup BoolDisj where
+  (BoolDisj False) <> (BoolDisj False) = BoolDisj False
+  _ <> _ = BoolDisj True
+
+instance Arbitrary BoolDisj where 
+  arbitrary = do
+    a <- elements [False, True]
+    return (BoolDisj a)
+
--- 15/14/maor/ChapterExercises/test/Spec.hs
+++ 15/14/maor/ChapterExercises/test/Spec.hs
@@ -14,4 +14,8 @@ main = do
   quickCheck (semigroupAssoc :: Trivial -> Trivial -> Trivial -> Bool)
   quickCheck (semigroupAssoc :: (Identity Trivial) -> (Identity Trivial) -> (Identity Trivial) -> Bool)
   quickCheck (semigroupAssoc :: (Identity MyInt) -> (Identity MyInt) -> (Identity MyInt) -> Bool)
-  
+  quickCheck (semigroupAssoc :: (Two Trivial MyInt) -> (Two Trivial MyInt) -> (Two Trivial MyInt) -> Bool)
+  quickCheck (semigroupAssoc :: (Three Trivial MyInt MyInt) -> (Three Trivial MyInt MyInt) -> (Three Trivial MyInt MyInt) -> Bool)
+  quickCheck (semigroupAssoc :: (Four Trivial MyInt MyInt Trivial) -> (Four Trivial MyInt MyInt Trivial) -> (Four Trivial MyInt MyInt Trivial) -> Bool)
+  quickCheck (semigroupAssoc :: BoolConj -> BoolConj -> BoolConj -> Bool)
+  quickCheck (semigroupAssoc :: BoolDisj -> BoolDisj -> BoolDisj -> Bool)
