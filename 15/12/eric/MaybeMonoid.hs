ea6a395506592f6c4c814b18ff298b0d495cbc24
Eric Dattore <edattore@gmail.com>
2016-09-19 09:29:42 -0600

    do exercises through section 12

---
+++ 15/12/eric/MaybeMonoid.hs
@@ -0,0 +1,58 @@
+module MaybeMonoid where
+
+import Test.QuickCheck
+import Control.Monad
+import Data.Monoid
+
+data Optional a =
+    Nada
+  | Only a
+  deriving (Eq, Show)
+
+instance Monoid a => Monoid (Optional a) where
+  mempty = Nada
+  mappend Nada x = x
+  mappend x Nada = x
+  mappend (Only x) (Only y) = Only (x <> y)
+
+newtype First' a =
+  First' { getFirst' :: Optional a }
+  deriving (Eq, Show)
+
+instance Monoid (First' a) where
+  mempty = First' Nada
+  mappend (First' Nada) (First' (Only a)) = First' (Only a)
+  mappend (First' (Only a)) _ = First' (Only a)
+  mappend (First' Nada) (First' Nada) = First' Nada
+
+firstMappend :: First' a -> First' a -> First' a
+firstMappend = mappend
+
+type FirstMappend =
+     First' String
+  -> First' String
+  -> First' String
+  -> Bool
+
+instance Arbitrary a => Arbitrary (First' a) where
+  arbitrary = do
+    a <- arbitrary
+    frequency [ (1, return (First' Nada))
+              , (1, return (First' (Only a)))
+              ]
+
+monoidAssoc :: (Eq m, Monoid m) => m -> m -> m -> Bool
+monoidAssoc a b c = (a <> (b <> c)) == ((a <> b) <> c)
+
+monoidLeftIdentity :: (Eq m, Monoid m) => m -> Bool
+monoidLeftIdentity m = (mempty <> m) == m
+
+monoidRightIdentity :: (Eq m, Monoid m) => m -> Bool
+monoidRightIdentity m = (m <> mempty) == m
+
+main :: IO ()
+main = do
+  quickCheck (monoidAssoc :: FirstMappend)
+  quickCheck (monoidLeftIdentity :: First' String -> Bool)
+  quickCheck (monoidRightIdentity :: First' String -> Bool)
+
