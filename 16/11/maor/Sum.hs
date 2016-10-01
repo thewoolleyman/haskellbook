75d030c1568d28f7c3b644f5c39452d586b996a0
Maor Leger <maor.leger@gmail.com>
2016-10-01 14:01:59 -0600

    ch 16

---
+++ 16/11/maor/Sum.hs
@@ -0,0 +1,12 @@
+module Sum where
+  data Sum a b =
+      First a
+    | Second b
+    deriving (Eq, Show)
+
+  instance Functor (Sum a) where
+    fmap _ (First a) = First a
+    fmap f (Second b) = Second (f b)
+
+  data LiftItOut f a = 
+    LiftItOut (f a)
