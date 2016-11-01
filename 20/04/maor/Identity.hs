970b09665f35ddad948996c715ca06d43ee070fc
Maor Leger <maor.leger@gmail.com>
2016-10-31 18:45:36 -0600

    20.04 Identity

---
+++ 20/04/maor/Identity.hs
@@ -0,0 +1,8 @@
+module Identity where
+
+data Identity a = Identity a
+
+instance Foldable Identity where
+  foldr f z (Identity x) = f x z
+  foldl f z (Identity x) = f z x
+  foldMap f (Identity x) = f x
