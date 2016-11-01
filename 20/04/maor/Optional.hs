ad7d4fb12e305fe77c3d58399b1ebb30d7c6afd5
Maor Leger <maor.leger@gmail.com>
2016-10-31 18:55:48 -0600

    20.04 Optional

---
+++ 20/04/maor/Optional.hs
@@ -0,0 +1,15 @@
+module Optional where
+
+data Optional a =
+    Nada
+  | Yep a
+
+instance Foldable Optional where
+  foldr _ z Nada = z
+  foldr f z (Yep a) = f a z
+
+  foldl _ z Nada = z
+  foldl f z (Yep a) = f z a
+
+  foldMap _ Nada = mempty
+  foldMap f (Yep a) = f a
