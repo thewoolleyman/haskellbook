59c5813e68c05d55f085fff67ec4724a76756404
Maor Leger <maor.leger@gmail.com>
2016-11-04 21:08:53 -0600

    20.06 more ch20 exercises

--- 20/06/maor/Ch20Exercises.hs
+++ 20/06/maor/Ch20Exercises.hs
@@ -12,15 +12,23 @@ data Two a b =
 
 instance Foldable (Two a) where
   foldr f z (Two _ b) = f b z
+  foldMap f (Two _ b) = f b
 
 data Three a b c =
   Three a b c
 
 instance Foldable (Three a b) where
   foldr f z (Three _ _ c) = f c z
+  foldMap f (Three _ _ c) = f c
 
 data Three' a b =
   Three' a b b
 
 instance Foldable (Three' a) where
-  foldMap f (Three' a b c) = f b <> f c
+  foldMap f (Three' _ b c) = f b <> f c
+
+data Four' a b =
+  Four' a b b b
+
+instance Foldable (Four' a) where
+  foldMap f (Four' _ b c d) = f b <> f c <> f d
