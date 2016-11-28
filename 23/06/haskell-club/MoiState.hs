94235cba2d780e7d71bf2ba9fa358fb538e96bbd
Eric Dattore <edattore@gmail.com>
2016-11-28 13:13:04 -0700

    add own state from haskell club

---
+++ 23/06/haskell-club/MoiState.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE InstanceSigs #-}
+module MoiState where
+
+newtype Moi s a =
+  Moi { runMoi :: s -> (a, s)  }
+
+instance Functor (Moi s) where
+  fmap :: (a -> b) -> Moi s a -> Moi s b
+  fmap f (Moi g) = Moi $ (\(a,s) -> (f a, s)) . g
+
+instance Applicative (Moi s) where
+  pure :: a -> Moi s a
+  pure a = Moi $ \s -> (a, s)
+  (<*>) :: Moi s (a -> b) -> Moi s a -> Moi s b
+  (Moi f) <*> (Moi g) = Moi $ (\(a, s) -> ((fst $ f s) a, s)) . g
+
+instance Monad (Moi s) where
+  return = pure
+  (>>=) :: Moi s a -> (a -> Moi s b) -> Moi s b
+  (>>=) (Moi f) g = Moi $ \s -> (runMoi $ (g . fst . f) s) s
+
