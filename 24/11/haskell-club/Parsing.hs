46e4f368622ab597b2978c6f6daf9e030f105b20
Eric Dattore <edattore@gmail.com>
2016-12-19 13:17:24 -0700

    rewrite parse method

--- 24/11/haskell-club/Parsing.hs
+++ 24/11/haskell-club/Parsing.hs
@@ -16,25 +16,32 @@ type Metadata = [NumberOrString]
 data SemVer =
   SemVer Major Minor Patch Release Metadata deriving (Show)
 
-parseRelease :: Parser Release
-parseRelease = do
-  char '-'
-  return [NOSS "n"]
+-- Yep, just what it sounds like ¯\_(ツ)_/¯
+parseNOSes :: Char -> Parser [NumberOrString]
+parseNOSes a = do
+  _ <- char a
+  p <- parseNOS
+  ps <- many $ char '.' >> parseNOS
+  return $ p : ps
+
+parseNOS :: Parser NumberOrString
+parseNOS =
+  fmap NOSI integer <|> fmap NOSS (many letter)
 
 parseSemVer :: Parser SemVer
-parseSemVer = do
-  maj <- integer
-  char '.'
-  minor <- integer
-  char '.'
-  patch <- integer
-  release <- (fmap (const []) eof <|> parseRelease)
-  -- many (char '-')
-  return $ SemVer maj minor patch release [(NOSS "")]
+parseSemVer = 
+  SemVer
+    <$> integer
+    <*> (char '.' >> integer)
+    <*> (char '.' >> integer)
+    <*> (fmap (const []) eof <|> parseNOSes '-')
+    <*> (fmap (const []) eof <|> parseNOSes '+')
 
 
 main :: IO ()
 main = do
   print $ parseString parseSemVer mempty "2.1.1"
   print $ parseString parseSemVer mempty "1.0.0-x.7.z.92"
+  --print $ parseString parseSemVer mempty "1.0.0+20130313144700"
+  print $ parseString parseSemVer mempty "1.0.0-beta+exp.sha.5114f85"
   --SemVer 2 1 1 [] [] > SemVer 2 10 [] []
