1413acf9b330671bed0adf26860d5f0bf2d991d2
Maor Leger <maor.leger@gmail.com>
2016-12-26 16:47:08 -0700

    26.03 EitherT Applicative

--- 26/03/maor/EitherT.hs
+++ 26/03/maor/EitherT.hs
@@ -5,3 +5,8 @@ newtype EitherT e m a =
 
 instance (Functor m) => Functor (EitherT e m) where
   fmap f (EitherT ema) = EitherT $ (fmap . fmap) f ema
+
+instance (Applicative m) => Applicative (EitherT e m) where
+  pure = EitherT . pure . Right
+  (EitherT fab) <*> (EitherT mea) =
+    EitherT $ (<*>) <$> fab <*> mea
