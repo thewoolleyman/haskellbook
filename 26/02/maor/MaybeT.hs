c52d62e3bae5111f6439cd0fa9ea54678215c0d0
Maor Leger <maor.leger@gmail.com>
2016-12-26 14:27:08 -0700

    26.02 MaybeT Monad

--- 26/02/maor/MaybeT.hs
+++ 26/02/maor/MaybeT.hs
@@ -11,3 +11,11 @@ instance (Functor m) => Functor (MaybeT m) where
 instance (Applicative m) => Applicative (MaybeT m) where
   pure = MaybeT . pure . pure
   (MaybeT fab) <*> (MaybeT mma) = MaybeT $ (<*>) <$> fab <*> mma
+
+instance (Monad m) => Monad (MaybeT m) where
+  return = pure
+  MaybeT ma >>= f =
+    MaybeT $ ma >>= \a ->
+      case a of
+        Nothing -> return Nothing
+        Just a -> runMaybeT . f $ a
