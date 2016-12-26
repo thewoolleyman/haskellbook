b24664e495df3f2a00e190ab6a548ea7215fdb52
Maor Leger <maor.leger@gmail.com>
2016-12-26 16:58:48 -0700

    26.03 EitherT Monad

--- 26/03/maor/EitherT.hs
+++ 26/03/maor/EitherT.hs
@@ -3,10 +3,18 @@ module EitherT where
 newtype EitherT e m a =
   EitherT { runEitherT :: m (Either e a) }
 
-instance (Functor m) => Functor (EitherT e m) where
+instance Functor m => Functor (EitherT e m) where
   fmap f (EitherT ema) = EitherT $ (fmap . fmap) f ema
 
-instance (Applicative m) => Applicative (EitherT e m) where
+instance Applicative m => Applicative (EitherT e m) where
   pure = EitherT . pure . Right
   (EitherT fab) <*> (EitherT mea) =
     EitherT $ (<*>) <$> fab <*> mea
+
+instance Monad m => Monad (EitherT e m) where
+  return = pure
+  EitherT mea >>= f =
+    EitherT $ mea >>= \ea ->
+      case ea of
+        Left e -> return $ Left e
+        Right a -> runEitherT $ f a
