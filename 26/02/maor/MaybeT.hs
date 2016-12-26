ef2f0eeb58a780bf62085f92c101f53b33374581
Maor Leger <maor.leger@gmail.com>
2016-12-26 14:23:54 -0700

    26.02 MaybeT Applicative

--- 26/02/maor/MaybeT.hs
+++ 26/02/maor/MaybeT.hs
@@ -6,3 +6,8 @@ newtype MaybeT m a =
 
 instance (Functor m) => Functor (MaybeT m) where
   fmap f (MaybeT ma) = MaybeT $ (fmap . fmap) f ma
+
+-- wow, I still dont understand this...
+instance (Applicative m) => Applicative (MaybeT m) where
+  pure = MaybeT . pure . pure
+  (MaybeT fab) <*> (MaybeT mma) = MaybeT $ (<*>) <$> fab <*> mma
