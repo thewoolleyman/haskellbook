e3a3dae2c96d5436a61a77411be17fd916402a52
Maor Leger <maor.leger@gmail.com>
2016-12-31 10:58:01 -0700

    26.04 ReaderT Applicative

--- 26/04/maor/ReaderT.hs
+++ 26/04/maor/ReaderT.hs
@@ -6,3 +6,7 @@ newtype ReaderT r m a =
 instance Functor m => Functor (ReaderT r m) where
   fmap f (ReaderT ma) =
     ReaderT $ (fmap . fmap)  f ma
+
+instance Applicative m => Applicative (ReaderT r m) where
+  pure  = ReaderT . pure . pure
+  ReaderT fma <*> ReaderT rm = ReaderT $ (<*>) <$> fma <*> rm
